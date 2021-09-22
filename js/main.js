2ce95d62e5f884ec32d975b5668ac2f05cc14e69
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2021-09-22 11:56:21 +0200

    tweaks to the nav

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('header nav ul li');
-const header = document.querySelector('header');
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("header nav ul li");
+const header = document.querySelector("header");
 
-window.addEventListener('scroll', ()=> {
-    let current = '';
+window.addEventListener("scroll", () => {
+  let current = "";
 
-    sections.forEach( section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if(pageYOffset >= (sectionTop - sectionHeight / 9.5)) {
-            current = section.getAttribute('id');
-        }
-    })
-
-    if (current < 10) {
-        header.classList.remove('headerShow')
-    } else {
-        header.classList.add('headerShow')
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 9.5) {
+      current = section.getAttribute("id");
     }
+  });
 
-console.log(current);
-    navLi.forEach( li => {
-        li.classList.remove('active')
-        if(li.classList.contains(current)){
-            li.classList.add('active')
-        }
-    })
-})
+  if (window.scrollY === 0) {
+    header.classList.remove("headerShow");
+    header.classList.add("showTwo");
+  } else {
+    header.classList.add("headerShow");
+    header.classList.remove("showTwo");
+  }
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
