8aa47526e79dca6cc6a213d289fbb043a3a038fe
Ahmed  Jibril <Ahmed.j.jibril@hotmail.com>
2021-10-30 22:12:48 +0200

    added a toggle function to the navigation on mobile and tablet

--- js/components/hamburger-menu.js
+++ js/components/hamburger-menu.js
@@ -1,4 +1,18 @@
-function toggleClass(){
-    let menu = document.querySelector(".menu");
-    menu.classList.toggle("togglecls")
+let homeBtn = document.querySelector(".home_container");
+let aboutBtn = document.querySelector(".about_container");
+let workBtn = document.querySelector(".work_container");
+let contactBtn = document.querySelector(".contact_container");
+let menu = document.querySelector(".menu");
+
+const hideMenuBtn = [];
+hideMenuBtn.push(homeBtn, aboutBtn, workBtn, contactBtn);
+
+function toggleClass() {
+  menu.classList.toggle("togglecls");
 }
+
+hideMenuBtn.forEach((btn) => {
+  btn.onclick = function () {
+    menu.classList.toggle("togglecls");
+  };
+});
